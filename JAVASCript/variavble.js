8b55af9e70839918a2d57d840fa3d42d596b8afa
ankit-mohanty <ankitmohanty347@gmail.com>
2024-02-18 17:13:46 +0530

    decalration of varible

---
+++ JAVASCript/variavble.js
@@ -0,0 +1,7 @@
+const account_id = 1435;
+let email = "ankit@gmail.com";
+var password = "Abhi";
+account_City = "BBSR";
+// account_id=2; It is not allowed because we declare as constant
+console.log(account_id);
+console.table([account_id,email,password,account_City]);// for calling multiple variable 
