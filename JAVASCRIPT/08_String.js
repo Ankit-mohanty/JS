7d1730aec8ee8086d150730c6c2aec94c9701872
ankit-mohanty <ankitmohanty347@gmail.com>
2024-04-19 10:14:36 +0530

    modification in String .js

--- JAVASCRIPT/08_String.js
+++ JAVASCRIPT/08_String.js
@@ -26,19 +26,19 @@ const mm = new String(' Ankit ');
 console.log(mm.substring(0, 4)); // here substring method used from which index number to which index number want to print. outpt : Anki index value is 0,1,2,3 only. 4 is not cunt in this function.
 // in substring function we can not pass -ve value if we pss the -ve value it will ount form the bigning.
 
-console.log(mm.slice(-5,3));// here  -5 means from last five characters and 3 means till three characters are showing. In slice and subString it is the main difference that we can pass -ve value in  slice. If we put -ve value in it it will count the index form reverse.
+console.log(mm.slice(-5, 3));// here  -5 means from last five characters and 3 means till three characters are showing. In slice and subString it is the main difference that we can pass -ve value in  slice. If we put -ve value in it it will count the index form reverse.
 
 
-const mm1= "  an  ";
+const mm1 = "  an  ";
 console.log(mm1);
 console.log(mm1.trim()); // here trim() method used for removing the white space from both sides of a string.
 
-console.log(mm.replace('n','t')); // replace() method is used to replace the specified value with another value.
+console.log(mm.replace('n', 't')); // replace() method is used to replace the specified value with another value.
 // in replace function first value is wchich value we ant to change and secoend is  what we want to put instead of that value.
 
 console.log(mm.includes(mm1)); // includes() method returns true or false whether the specified string present in the main string.
 
-const mm2= "ankit-mohanty-rahul";// i want to convert this string to array based on  "-". We can do anything insead of the '-'.
+const mm2 = "ankit-mohanty-rahul";// i want to convert this string to array based on  "-". We can do anything insead of the '-'.
 console.log(mm2.split('-'));// we use this function to conavert a string to array.
 
 console.log(mm2.charAt(1));
@@ -52,9 +52,10 @@ console.log(mm2.startsWith('a'));// this mathod chack the method  that our strin
 console.log(mm2.endsWith('a'));// This method  check that our string ends with or not.  this method return typpe is BOOLEAN
 
 // console.log(mm2.padStart(25));//this function add Hi at the starting side of the string.iF WE DO not pass any value in it it will pass use "<space >" to incresh the string length.
-console.log(mm2.padEnd(21,'hi'));// this function add Hi at the ending side of the string.iF WE DO not pass any value in it it will pass use "<space >" to incresh the string length.
+console.log(mm2.padEnd(21, 'hi'));// this function add Hi at the ending side of the string.iF WE DO not pass any value in it it will pass use "<space >" to incresh the string length.
 console.log(mm2.length);
 
 console.log(mm.repeat(5));// this mwthod is use to repeat the value of it.
 
-console.log(mm.search(/[A-Z]/g));
+console.log(mm2.search("n"));// This returns index value of the giver charecter..
+
