daa9396a9d1975906b9911a412ab7accd2ff65bd
ankit-mohanty <ankitmohanty347@gmail.com>
2024-04-27 14:31:49 +0530

    while loop and do..while loope complite

---
+++ 005_ITARATION/02_loop.js
@@ -0,0 +1,18 @@
+let index = 0;
+while (index <= 10) {
+    console.log(`value is ${index}`);
+    index = index + 2;
+}
+
+let myArr = ["In=ronman", "BatMan", "SuperMan"];
+let arr = 0
+while (arr < myArr.length) {
+    console.log(`My value is ${myArr[arr]}`);
+    arr = arr + 1;
+}
+
+let score =0;
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score<=10);
