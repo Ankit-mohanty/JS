d15c7d91165fd741ae1f3580eb37673851bc32ab
Ankit-Mohanty <ankitmohanty347@gmail.com>
2025-02-22 21:11:34 +0530

    bind

--- JAVASCRIPT/015_CLASS_CONSTRUCTOR/staticcrop.js
+++ JAVASCRIPT/015_CLASS_CONSTRUCTOR/staticcrop.js
@@ -1,22 +1,19 @@
-class User{
-    constructor(username){
-        this.username=username
-    
+class User {
+  constructor(username) {
+    this.username = username;
+  }
 
-    }
+  logMe() {
+    console.log(`User is  ${this.username}`);
+  }
 
-
-    logMe(){
-        console.log(`User is  ${this.username}`)
-                }
-
-                // i do not want to give the access all the methods
-               // 
-                // so i am using the prototype
-            static    createId(){
-                    return `123`
-                }
+  // i do not want to give the access all the methods
+  //
+  // so i am using the prototype
+  static createId() {
+    return `123`;
+  }
 }
 
 const us = new User("Ankit");
-console.log(us.createId())
+console.log(us.createId());
